72e19d2a764805a5b8a71d695695849ca6bf1bcf
Pratik Sharma <pratik.sharma_cs19@gla.ac.in>
2020-10-28 22:56:26 +0530

    finding difference and printing it.

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,11 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        int currentDate = new GregorianCalendar().get(Calendar.DATE);
+        String currentMonth = new GregorianCalendar().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
+        int totalNumOfDays = new GregorianCalendar().getActualMaximum(Calendar.DAY_OF_MONTH);
+        int noOfDaysLeft = totalNumOfDays - currentDate;
+        System.out.println(noOfDaysLeft + " days left in " + currentMonth + "!");
 
     }
 
