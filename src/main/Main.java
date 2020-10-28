a76f5bc0057243e3bb8b2b2b391068939bb47894
Pratik Sharma <pratik.sharma_cs19@gla.ac.in>
2020-10-28 22:33:19 +0530

    create Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Pratik Sharma (pratik121212)
+ *   Date: 28-10-2020
+ *   Time: 22:32
+ *   File: Main.java
+ */
+
+package main;
+
+public class Main {
+
+}
