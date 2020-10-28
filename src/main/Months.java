29f57015a5e9311775da497d02a9962e508b6f98
Pratik Sharma <pratik.sharma_cs19@gla.ac.in>
2020-10-28 22:31:52 +0530

    create an enum named Months and create a constructor for it.

--- src/main/Months.java
+++ src/main/Months.java
@@ -1,5 +1,16 @@
 package main;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
-public enum Months {
-    
+enum Months {
+    JANUARY(31), FEBRUARY(29), MARCH(31), APRIL(30), MAY(31),
+    JUNE(30), JULY(31), AUGUST(31), SEPTEMBER(30), OCTOBER(31),
+    NOVEMBER(30), DECEMBER(31);
+
+    private final int numOfDays;
+
+    Months(int numOfDays){
+        this.numOfDays = numOfDays;
+    }
 }
